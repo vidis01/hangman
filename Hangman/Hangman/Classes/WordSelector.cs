42f6c153f6377fff03a9311c1e7f1bf660f02220
Vidmantas <jaguaras999@gmail.com>
2024-10-02 18:58:52 +0300

    added protected

--- Hangman/Hangman/Classes/WordSelector.cs
+++ Hangman/Hangman/Classes/WordSelector.cs
@@ -6,7 +6,7 @@ namespace Hangman.Classes
     public class WordSelector : IWordSelector
     {
         private Random _random;
-        private Dictionary<TopicEnum, string[]> _words;
+        protected Dictionary<TopicEnum, string[]> _words;
         
         public WordSelector(Random random)
         {
@@ -17,7 +17,8 @@ namespace Hangman.Classes
                 { TopicEnum.COUNTRIES, new string[] { "Latvia", "Estonia", "Denmark", "China", "Ireland", "Thailand", "Zimbabwe", "France", "Uganda", "Poland", "Sweden" } },
                 { TopicEnum.CITIES, new string[] { "Vilnius", "Kaunas", "Venice", "Kedainiai", "Paris", "Mumbai", "Philadelphia", "Dallas", "Kolkata", "Pasvalys", "Madrid" } },
                 { TopicEnum.SEAS, new string[] { "Java", "Solomon", "White", "Sargasso", "Baltic", "Celebes", "labrador", "Norwegian", "Weddell", "Caribbean", "Greenland" } }            
-            };            
+            };
+
         }
 
         public string SelectWord(TopicEnum? topic)
